769f6f5cf0d3843d90a7b0dd32b605d67a36e2f9
Stemesies <stemesies@gmail.com>
2026-05-21 21:19:57 +0700

    Alu 2

--- lab5/model_alu.sv
+++ lab5/model_alu.sv
@@ -1,3 +1,41 @@
+module module_AND (
+output logic out,
+input logic in1,
+input logic in2,
+input logic c_in
+);
+
+assign out = (in1 & in2) || (c_in & (in1 || in2));
+endmodule
+
+module module_XOR (
+output logic out,
+input logic in1,
+input logic in2,
+input logic c_in
+);
+
+assign out = in1 ^ in2 ^ c_in;
+endmodule
+
+module adder (
+input logic na, nb, c_in,
+output logic nsum, nc_out
+);
+
+logic a, b, sum, c_out;
+
+assign a = na;
+assign b = nb;
+
+module_XOR m1(.out(sum), .in1(a), .in2(b), .c_in(c_in));
+module_AND m2(.out(c_out), .in1(a), .in2(b), .c_in(c_in));
+
+assign nsum = sum;
+assign nc_out = c_out;
+
+endmodule
+
 module alu(
     input logic [3:0] a, rawb,
     input logic [2:0] funcsel,
@@ -11,7 +49,6 @@ always_comb begin
     case (funcsel[1:0])
         default: 
     endcase
-
 end
 
 endmodule
