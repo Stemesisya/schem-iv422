ab7d66cd32de08557cb88fa3284aef1d18b3ab43
Stemesies <stemesies@gmail.com>
2026-05-21 15:36:06 +0700

    Alu 1

---
+++ lab5/model_alu.sv
@@ -0,0 +1,17 @@
+module alu(
+    input logic [3:0] a, rawb,
+    input logic [2:0] funcsel,
+    output logic [3:0] result
+);
+
+logic [3:0] b;
+assign b = 1
+
+always_comb begin
+    case (funcsel[1:0])
+        default: 
+    endcase
+
+end
+
+endmodule
