fe30accd30bda37edc3a602d35bd947573a1061a
WizLoU <vorkunovk@gmail.com>
2026-05-25 20:22:33 +0700

    Add bit_adder and 4-bit ripple carry adder

--- lab5/model_alu.sv
+++ lab5/model_alu.sv
@@ -1,3 +1,24 @@
+module bit_adder (
+input logic a, b, c_in,
+output logic sum, c_out
+);
+assign sum = a ^ b ^ c_in;
+assign c_out = (a & b) | (c_in & (a | b));
+endmodule
+
+module new_adder(
+    input logic [3:0] a, b,
+    input logic c_in,
+    output logic [3:0] sum,
+    output logic c_out
+);
+logic c_out0, c_out1, c_out2;
+bit_adder a1(.sum(sum[0]), .c_out(c_out0),  .a(a[0]), .b(b[0]), .c_in(c_in));
+bit_adder a2(.sum(sum[1]), .c_out(c_out1),  .a(a[1]), .b(b[1]), .c_in(c_out0));
+bit_adder a3(.sum(sum[2]), .c_out(c_out2),  .a(a[2]), .b(b[2]), .c_in(c_out1));
+bit_adder a4(.sum(sum[3]), .c_out(c_out),  .a(a[3]), .b(b[3]), .c_in(c_out2));
+endmodule
+
 module module_AND (
 output logic out,
 input logic in1,
@@ -18,7 +39,7 @@ input logic c_in
 assign out = in1 ^ in2 ^ c_in;
 endmodule
 
-module adder (
+module old_adder (
 input logic na, nb, c_in,
 output logic nsum, nc_out
 );
@@ -43,13 +64,18 @@ module alu(
 );
 
 logic [3:0] b;
+logic [3:0] sum;
+logic c_out;
+
 assign b = rawb;
+
+new_adder adderr(.a(a), .b(b), .sum(sum), .c_out(c_out), .c_in(1'b0));
 
 always_comb begin
     case (funcsel[1:0])
         2'b00: result = a & b;
         2'b01: result = a | b;
-        2'b10: result = a + b;
+        2'b10: result = sum;
         2'b11: result = 4'b0000;
         default: result = 4'b0000;
     endcase
