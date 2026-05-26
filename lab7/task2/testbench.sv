8390a007e5dd87bebebc7c97552c5a3d5d1e28eb
Stemesies <stemesies@gmail.com>
2026-05-26 20:27:50 +0700

    Done (lab7:t2)

---
+++ lab7/task2/testbench.sv
@@ -0,0 +1,88 @@
+module lab7_t2_test();
+    logic clk, reset, button, ol;
+    logic [3:0] q;
+    
+    always begin
+        clk = 0; #5;
+        clk = 1; #5;
+    end
+
+	counter4b_step4 c4(.clk(clk), .reset(reset), .rawButton(button), .q(q), .ol(ol));
+    
+    initial begin
+
+        reset = 1;
+		button = 0;
+		#5;
+        reset = 0;
+		#20;
+        $display("q: %b (Expected: 0000)", q); // Сброс
+		#20000000;
+        $display("q: %b (Expected: 0000)", q); // Не должно автоинкрементироваться
+
+		button = 1;
+		#5;
+		button = 0;
+		#5;
+		button = 1;
+		#5;
+		button = 0;
+		#5;
+		button = 1;
+		#5;
+		button = 0;
+		#5;
+		$display("q: %b, ol: %b (Expected: 0000, 0)", q, ol); // Дребезг не должен учесться
+		 
+        button = 1; 
+        #20000000;
+        $display("q: %b, ol: %b (Expected: 0100, 0)", q, ol); // Клик
+        button = 0; 
+        #20000000;
+
+		button = 1; 
+        #20000000;
+        $display("q: %b, ol: %b (Expected: 1000, 0)", q, ol); // Клик
+        button = 0; 
+        #20000000;
+
+		button = 1; 
+        #20000000;
+        $display("q: %b, ol: %b (Expected: 1100, 0)", q, ol); // Клик
+        button = 0; 
+        #20000000;
+
+		button = 1; 
+        #20000000;
+        $display("q: %b, ol: %b (Expected: 0000, 0)", q, ol); // Клик
+        button = 0; 
+        #20000000;
+
+		button = 1; 
+        #20000000;
+        $display("q: %b, ol: %b (Expected: 0100, 0)", q, ol); // Клик
+        button = 0; 
+        #20000000;
+
+		button = 1; 
+        #20000000;
+        $display("q: %b, ol: %b (Expected: 1000, 0)", q, ol); // Клик
+        button = 0; 
+        #20000000;
+
+		button = 1;
+		reset = 1;
+        #40;
+        $display("q: %b (Expected: 0000)", q); // Сброс
+        button = 0;
+        #20000000;
+		$display("q: %b (Expected: 0000)", q);
+        button = 0;
+        #20000000;
+		 
+		 
+
+        $stop;
+    end
+    
+endmodule
