08a40154388a233da619ea38962bd5bad9fcea05
Stemesies <stemesies@gmail.com>
2026-05-26 19:02:59 +0700

    Now tests are working (lab7:t1)

--- lab7/task1/testbench.sv
+++ lab7/task1/testbench.sv
@@ -1,4 +1,4 @@
-module tb_counter4();
+module lab7_t1_test();
     logic clk, reset, button, ol;
     logic [3:0] q;
     
@@ -17,7 +17,7 @@ module tb_counter4();
         reset = 0;
 		#20;
         $display("q: %b (Expected: 0000)", q); // Сброс
-		#99999;
+		#20000000;
         $display("q: %b (Expected: 0000)", q); // Не должно автоинкрементироваться
 
 		button = 1;
@@ -35,122 +35,122 @@ module tb_counter4();
 		$display("q: %b, ol: %b (Expected: 0000, 0)", q, ol); // Дребезг не должен учесться
 		 
         button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0001, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0010, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0011, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0100, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0101, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0110, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0111, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 1000, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 1001, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 1010, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 1011, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 1100, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 1101, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 1110, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 1111, 1)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0000, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0001, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1; 
-        #99999;
+        #20000000;
         $display("q: %b, ol: %b (Expected: 0010, 0)", q, ol); // Клик
         button = 0; 
-        #99999;
+        #20000000;
 
 		button = 1;
 		reset = 1;
         #40;
         $display("q: %b (Expected: 0000)", q); // Сброс
         button = 0;
-        #99999;
+        #20000000;
 		$display("q: %b (Expected: 0000)", q);
         button = 0;
-        #99999;
+        #20000000;
 		 
 		 
 
