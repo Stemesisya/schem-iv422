224e3838bc0b6b5e49979eb2270fdd0d4b604322
Stemesies <stemesies@gmail.com>
2026-05-26 17:13:05 +0700

    Counter created (lab7:t1)

---
+++ lab7/task1.sv
@@ -0,0 +1,41 @@
+module antitinkling (
+    input logic button,
+    input logic clk,
+    input logic reset,
+    output logic out
+);
+
+    logic c;
+    logic [19:0] q;
+
+    always_ff @(posedge c)
+        out <= button;
+
+    always_ff @(posedge clk, posedge reset)
+        if (reset) q <= 0;
+        else q <= q + 1;
+
+    assign c = (q == 20'hFFFFF);
+
+endmodule
+
+
+module counter4b (
+    input logic clk,
+    input logic reset,
+    input logic rawButton,
+    output logic [3:0] q,
+    output logic ol
+);
+    logic button;
+
+    antitinkling at(rawButton, clk, reset, clkInternal);
+
+    always_ff @(posedge button) begin
+        if (reset)q <= 0;
+        else q <= q + 1;
+    end
+
+    assign ol = (q==4'b1111);
+
+endmodule
