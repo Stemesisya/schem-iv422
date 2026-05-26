515c1409cbcd740366fd7524a2857d307f812e68
Stemesies <stemesies@gmail.com>
2026-05-26 10:38:47 +0700

    Impulse Redistributor made (lab6)

---
+++ lab6/task4/module_impulse_redistributor.sv
@@ -0,0 +1,31 @@
+// task 5
+
+module module_flop_r(
+    input logic clk,
+    input logic reset,
+    input logic d,
+    output logic q
+);
+
+always_ff @(posedge clk) begin
+    if (reset) q <=0;
+    else q <= d;
+end
+
+endmodule
+
+module module_impulse_redistributor(
+    input logic clk,
+    input logic reset,
+    output [3:0] logic out
+)
+
+logic d;
+assign d = ~out[0] & ~out[1] & ~out[2] & ~out[3];
+
+module_flop_r f1(.clk(clk), .reset(reset), .d(  d   ), .q(out[0]));
+module_flop_r f2(.clk(clk), .reset(reset), .d(out[0]), .q(out[1]));
+module_flop_r f3(.clk(clk), .reset(reset), .d(out[1]), .q(out[2]));
+module_flop_r f4(.clk(clk), .reset(reset), .d(out[2]), .q(out[3]));
+
+endmodule
---
+++ lab6/task4/testbench.sv
@@ -0,0 +1,48 @@
+module test();
+	logic clk,reset;
+	logic tick, reset, [3:0] result;
+	logic resultExpected;
+	logic [100:0] testIndex, errors;
+	logic [5:0] testvectors [100:0];
+
+	module_flop m1(.clk(tick), .d(d), .q(result));
+	
+	always begin
+		clk=1; #5; clk=0; #5;
+	end
+
+	initial begin
+
+		reset = 1;
+        tick = 1; // Сбрасываем счетчик, чтобы он показывал 0, а не X
+
+		$readmemb ("lab6/task4/test.txt", testvectors);
+		testIndex =0;
+		errors=0;
+		reset =1; #27; reset=0;
+	end
+
+	always @(posedge clk) begin
+		#1; {tick, reset, resultExpected} = testvectors [testIndex];
+	end
+
+	always @(negedge clk)
+	begin
+		if (~reset)
+		begin
+			if (result!==resultExpected)
+			begin
+				$display("[%d] Error result: inputs = d:%b, clk:%b", testIndex, d, tick);
+				$display(" result = %b (%b expected)", result, resultExpected);
+				errors = errors+1;
+			end
+			
+			testIndex = testIndex+1;
+			if (testvectors[testIndex]===6'bx) // testVector length + 1
+			begin
+				$display("%d tests completed with %d errors", testIndex, errors);
+				$stop;
+			end
+		end
+	end
+endmodule
